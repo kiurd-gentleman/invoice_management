beaabc3b59136b2b5613475040edd5d48000e273
kiurd-gentelman <k.r.imtiaz@gmail.com>
2021-06-17 16:58:28 +0600

    database finding

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Foxtrot'),
+    'name' => env('APP_NAME', 'invoice_management'),
 
     /*
     |--------------------------------------------------------------------------
